b0a3e1d0923262b4fc5ef861845153664438e855
Gavin Peterson <gavinpeterson@er-10-132-226-1.wireless.umn.edu>
2018-04-21 12:53:31 -0500

    creating list of employees

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,18 +11,14 @@
   
   var database = firebase.database();
 
-  var Name;
-  var Role;
-  var startDate;
-  var monthlyRate;
 
   $('#add-user').on('click', function() {
     event.preventDefault();
 
-    employeeName = $('#name-input').val().trim();
-    employeeRole = $('#role-input').val().trim();
-    employeeStartDate = $('#date-input').val().trim();
-    employeeMonthlyRate = $('#rate-input').val().trim();
+    let employeeName = $('#name-input').val().trim();
+    let employeeRole = $('#role-input').val().trim();
+    let employeeStartDate = $('#date-input').val().trim();
+    let employeeMonthlyRate = $('#rate-input').val().trim();
 
     $('#name-input').val('');
     $('#role-input').val('');
@@ -38,3 +34,25 @@
 
 
   })
+
+database.ref().on('child_added', function(childSnapshot){
+
+  let snapName = childSnapshot.val().name;
+  let snapRole = childSnapshot.val().role;
+  let snapStartDate = childSnapshot.val().startDate;
+  let snapMonthlyRate = childSnapshot.val().monthlyRate;
+  let currentDate = new Date();
+  console.log(currentDate);
+  
+  let newRow = $('<tr>')
+
+  let nameCell = $('<td class="employee-name">').text(snapName)
+  let roleCell = $('<td class="employee-role">').text(snapRole)
+  let startCell = $('<td class="employee-start-date">').text(snapStartDate)
+  let rateCell = $('<td class="employee-monthly-rate">').text(snapMonthlyRate)
+  
+  newRow.append(nameCell).append(roleCell).append(startCell).append(rateCell)
+  $('#current-employees').append(newRow);
+
+  
+})
