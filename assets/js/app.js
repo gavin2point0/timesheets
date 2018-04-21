1a3714a73ca0e37bb79723c64798fb824e033d04
Gavin Peterson <gavinpeterson@er-10-132-226-1.wireless.umn.edu>
2018-04-21 10:26:43 -0500

    linking files and adding firebase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB8wocc44m8SPDLmqkzbLYKqp6Q-hDTGI0",
+    authDomain: "timesheets-demo.firebaseapp.com",
+    databaseURL: "https://timesheets-demo.firebaseio.com",
+    projectId: "timesheets-demo",
+    storageBucket: "",
+    messagingSenderId: "983750965131"
+  };
+  firebase.initializeApp(config);
